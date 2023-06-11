adc82b39a3376f19d0f0aa657db37de5644fa799
Andriy Dohniak <pavlofort@gmail.com>
2023-06-11 23:37:31 +0200

    Final laptop

--- ex00/backtrack.c
+++ ex00/backtrack.c
@@ -13,40 +13,41 @@ int	handle_args(int argc, char **argv, int sums[4][N])
 	int	row;
 	int	column;
 
-    arg_iter = 0;
+	arg_iter = 0;
 	if (argc != 2)
 	{
 		print_error();
-		return -1;
+		return (-1);
 	}
 	row = 0;
 	column = 0;
-	while (arg_iter < N*4*2 - 1)
+	while (arg_iter < N * 4 * 2 - 1)
 	{
 		if (argv[1][arg_iter] == '\0')
 		{
 			print_error();
-			return -1;
+			return (-1);
 		}
 		if (argv[1][N * 8 - 1] != '\0')
 		{
 			print_error();
-			return -1;
+			return (-1);
 		}
-
-		if (arg_iter % 2 == 0 && argv[1][arg_iter] >= 49 && argv[1][arg_iter] < 49+N){
-		   sums[row][column] = argv[1][arg_iter] - '0';
-		   column++;
-		   if (column == N){
+		if (arg_iter % 2 == 0 && argv[1][arg_iter] >= 49
+			&& argv[1][arg_iter] < 49 + N)
+		{
+			sums[row][column] = argv[1][arg_iter] - '0';
+			column++;
+			if (column == N)
+			{
 				column = 0;
 				row++;
-		   }
+			}
 		}
 		else if (arg_iter % 2 != 1 || argv[1][arg_iter] != ' ')
 		{
 			print_error();
-
-			return -1;
+			return (-1);
 		}
 		arg_iter++;
 	}
@@ -65,95 +66,93 @@ void	print(int fields[N][N])
 	{
 		while (column < N)
 		{
-            if (fields[row][column] >= 0 && fields[row][column] <= N)
-                temp = '0' + fields[row][column];
-            else
-                temp = '-';
-			//write (1, &temp, 1);
-            printf("%c", temp);
+			if (fields[row][column] >= 0 && fields[row][column] <= N)
+				temp = '0' + fields[row][column];
+			else
+				temp = '-';
+			write(1, &temp, 1);
 			if (column != N - 1)
-				//write(1, " ", 1);
-                printf(" ");
+				write(1, " ", 1);
 			column++;
 		}
 		row++;
 		column = 0;
-		//write(1, "\n ", 1);
-        printf("\n");
+		write(1, "\n ", 1);
 	}
-    //write(1, "\n", 1);
-    printf("\n");
 }
 
 int	is_safe_rn(int fields[N][N], int sums[4][N], int column, int row)
 {
 	if (0 != check_num_vertical(fields, fields[row][column])
-		|| 0 != check_num_horisontal(fields, fields[row][column])){
-		return -1;
+		|| 0 != check_num_horisontal(fields, fields[row][column]))
+	{
+		return (-1);
 	}
-	if (column == N-1){
-		if(0 != check_left_to_righ(fields, sums, row)
+	if (column == N - 1)
+	{
+		if (0 != check_left_to_righ(fields, sums, row)
 			|| 0 != check_right_to_left(fields, sums, row))
 		{
 			return (-1);
 		}
 	}
-	if (row == N-1)
+	if (row == N - 1)
 	{
-		if ( 0 != check_top_to_bottom(fields, sums, column)
+		if (0 != check_top_to_bottom(fields, sums, column)
 			|| 0 != check_bottom_to_top(fields, sums, column))
 		{
 			return (-1);
 		}
 	}
-	return 0;
+	return (0);
 }
 int	backtrack(int fields[N][N], int sums[4][N], int column, int row)
 {
-	int success;
-	int num;
+	int	success;
+	int	num;
+
 	num = fields[row][column];
-	if (column >= N){
+	if (column >= N)
+	{
 		column = 0;
 		row++;
 	}
-	print(fields);
-
-	if(0 != is_safe_rn(fields, sums, column, row))
-		return -1;
-	if (row == N-1 && column == N-1)
+	if (0 != is_safe_rn(fields, sums, column, row))
+		return (-1);
+	if (row == N - 1 && column == N - 1)
 	{
-        //write(1, "Done\n", 5);
-        printf("Done \n");
-		return 0;
+		return (0);
 	}
 	num = 1;
 	column++;
 	while (num <= N)
 	{
 		fields[row][column] = num;
-		if(0 == backtrack(fields, sums, column, row))
-			return 0;
+		if (0 == backtrack(fields, sums, column, row))
+			return (0);
 		num++;
 	}
 	fields[row][column] = 0;
-	return -1;
+	return (-1);
 }
 int	main(int argc, char **argv)
 {
-	int fields[N][N] = {0};
-	int sums[4][N];
-    int test[N][N] = {{1, 3, 2, 4, 5}};
+	int	fields[N][N];
+	int	sums[4][N];
+	int	iter;
 
+	iter = 0;
+	zero_out_N_N(fields, N, N);
+	zero_out_4_N(sums, 4, N);
 	if (0 != handle_args(argc, argv, sums))
-		return -1;
-    printf("Res %i\n", check_left_to_righ(test, sums, 0));
-	print(sums);
-	for(int i = 1; i<=N; i++){
+		return (-1);
+	for (int i = 1; i <= N; i++)
+	{
 		fields[0][0] = i;
-		if (0 == backtrack(fields, sums, 0, 0)){
-			//print(fields);
-			return 0;
+		if (0 == backtrack(fields, sums, 0, 0))
+		{
+			print(fields);
+			return (0);
 		}
 	}
 	print_error();
--- ex00/check.h
+++ ex00/check.h
@@ -1,11 +1,57 @@
 #define N 5
-#include <stdio.h>
 
+void	zero_out_N_N(int arr[N][N], int x, int y)
+{
+	int	col;
+	int	row;
+
+	col = 0;
+	row = 0;
+	while (row < y)
+	{
+		while (col < x)
+		{
+			arr[row][col] = 0;
+			col++;
+		}
+		row++;
+	}
+}
+
+void	zero_out_4_N(int arr[4][N], int x, int y)
+{
+	int	col;
+	int	row;
+
+	col = 0;
+	row = 0;
+	while (row < y)
+	{
+		while (col < x)
+		{
+			arr[row][col] = 0;
+			col++;
+		}
+		row++;
+	}
+}
+
+void	zero_out_single(int arr[N], int x)
+{
+	int	col;
+
+	col = 0;
+	while (col < x)
+	{
+		arr[col] = 0;
+		col++;
+	}
+}
 int	check_left_to_righ(int fields[N][N], int sums[4][N], int row)
 {
-	int column;
-	int current_can_see;
-	int highest;
+	int	column;
+	int	current_can_see;
+	int	highest;
 
 	current_can_see = 0;
 	highest = 0;
@@ -20,18 +66,15 @@ int	check_left_to_righ(int fields[N][N], int sums[4][N], int row)
 		column++;
 	}
 	if (current_can_see != sums[2][row])
-    {
-        printf("Check l2r %i %i\n", current_can_see, sums[2][row]);
 		return (-1);
-    }
 	return (0);
 }
 
-int check_right_to_left(int fields[N][N], int sums[4][N], int row)
+int	check_right_to_left(int fields[N][N], int sums[4][N], int row)
 {
-	int column;
-	int current_can_see;
-	int highest;
+	int	column;
+	int	current_can_see;
+	int	highest;
 
 	current_can_see = 0;
 	highest = 0;
@@ -46,18 +89,15 @@ int check_right_to_left(int fields[N][N], int sums[4][N], int row)
 		column--;
 	}
 	if (current_can_see != sums[3][row])
-    {
-        printf("Check r2l %i\n", current_can_see);
 		return (-1);
-    }
 	return (0);
 }
 
-int check_top_to_bottom(int fields[N][N], int sums[4][N], int column)
+int	check_top_to_bottom(int fields[N][N], int sums[4][N], int column)
 {
-	int row;
-	int current_can_see;
-	int highest;
+	int	row;
+	int	current_can_see;
+	int	highest;
 
 	current_can_see = 0;
 	highest = 0;
@@ -72,18 +112,15 @@ int check_top_to_bottom(int fields[N][N], int sums[4][N], int column)
 		row++;
 	}
 	if (current_can_see != sums[0][column])
-    {
-        printf("Check top to bottom\n");
 		return (-1);
-    }
 	return (0);
 }
 
-int check_bottom_to_top(int fields[N][N], int sums[4][N], int column)
+int	check_bottom_to_top(int fields[N][N], int sums[4][N], int column)
 {
-	int row;
-	int current_can_see;
-	int highest;
+	int	row;
+	int	current_can_see;
+	int	highest;
 
 	current_can_see = 0;
 	highest = 0;
@@ -98,33 +135,28 @@ int check_bottom_to_top(int fields[N][N], int sums[4][N], int column)
 		row--;
 	}
 	if (current_can_see != sums[1][column])
-    {
-        printf("Check bottom to top\n");
 		return (-1);
-    }
 	column++;
 	return (0);
 }
 
-int check_num_horisontal(int fields[N][N], int num)
+int	check_num_horisontal(int fields[N][N], int num)
 {
 	int	row;
 	int	column;
-	int	total[N] = {0};
+	int	total[N];
 
+	zero_out_single(total, N);
 	row = 0;
-	while(row < N)
+	while (row < N)
 	{
 		column = 0;
-		while(column < N)
+		while (column < N)
 		{
 			if (fields[row][column] == num)
 			{
 				if (fields[row][column] == total[column])
-                {
-                    printf("Check num horizontal\n");
-					return -1;
-                }
+					return (-1);
 				total[column] = fields[row][column];
 			}
 			column++;
@@ -134,25 +166,23 @@ int check_num_horisontal(int fields[N][N], int num)
 	return (0);
 }
 
-int check_num_vertical(int fields[N][N], int num)
+int	check_num_vertical(int fields[N][N], int num)
 {
 	int	row;
 	int	column;
-	int	total[N] = {0};
+	int	total[N];
 
+	zero_out_single(total, N);
 	column = 0;
-	while(column < N)
+	while (column < N)
 	{
 		row = 0;
-		while(row < N)
+		while (row < N)
 		{
 			if (fields[row][column] == num)
 			{
 				if (fields[row][column] == total[row])
-                {
-                    printf("Check num vertical\n");
-					return -1;
-                }
+					return (-1);
 				total[row] = fields[row][column];
 			}
 			row++;
